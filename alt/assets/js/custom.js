ba50e653412220c81c81c796f687491e390612b3
nzhulikov <nzhulikov@gmail.com>
2016-09-23 12:27:36 +0300

    minor fixes
    + reorganized code
    + add boilerplate code for releases.html page

--- alt/assets/js/custom.js
+++ alt/assets/js/custom.js
@@ -1,70 +1,44 @@
-(function ($) {
-  // custom contact form submit (redirect letter)
-  $( "#contact-form" ).submit(function( event ) {
+// map overlay
+var disableMapOverlay = function ($overlay) {
+  $overlay.fadeOut('fast')
+    .addClass('no-overlay');
+};
 
-    // Stop form from submitting normally
-    event.preventDefault();
-
-    var $submitButton = $("#contact-form input[type='submit']").addClass("disabled");
-
-    // Get some values from elements on the page:
-    var $form = $( this ),
-    element_0 = $form.find( "input[name='element_0']" ).val(),
-    element_1 = $form.find( "input[name='element_1']" ).val(),
-    element_2 = $form.find( "textarea[name='element_2']" ).val(),
-    element_counts = $form.find( "input[name='element_counts']" ).val(),
-    embed = $form.find( "input[name='embed']" ).val(),
-    url = $form.attr( "action" );
-
-    // Send the data using post
-    var posting = $.post( url, { element_0: element_0, element_1: element_1, element_2: element_2, element_counts: element_counts, embed: embed })
-    .always(function () {
-      $form.addClass("disabled");
-      $submitButton.val("Отправлено");
-      $submitButton.addClass("success");
-      $form.find("input[type='reset']").addClass("hidden");
-      $form.find(".success-message").removeClass("hidden");
-    })
-    .done(function (data) {
-      $submitButton.val("Получено");
+var enableMapOverlay = function ($overlay) {
+  if ($overlay.hasClass('no-overlay')) {
+    $overlay.fadeIn('slow', function () {
+      $overlay.removeClass('no-overlay');
     });
-  });
+  }
+};
 
-  // map overlay
-  var disableMapOverlay = function ($overlay) {
-    $overlay.fadeOut("fast")
-      .addClass("no-overlay");
-  };
+// form autofill by links
+var fillFormByLink = function($link) {
+  var $form = $($link.attr('href')); // works only if href represents #id of some form
+  var fillTarget = $link.attr('fill-target');
+  var fillData = $link.attr('fill-data');
 
-  var enableMapOverlay = function ($overlay) {
-    if ($overlay.hasClass("no-overlay")) {
-      $overlay.fadeIn("slow", function () {
-        $overlay.removeClass("no-overlay");
-      });
-    }
-  };
+  var $target = $form.find('textarea[name=' + fillTarget + ']'); // get target element of the form
+  var data = $('#' + fillData).text();
 
-  $(".map-overlay").click(function () {
+  $target.text(data);
+
+  setTimeout(function () {
+    $form.find('input:first').focus();
+  }, 1500);
+};
+
+(function ($) {
+
+  $('.map-overlay').click(function () {
     disableMapOverlay($(this));
   });
-  $(".map-wrapper").mouseleave(function () {
+  $('.map-wrapper').mouseleave(function () {
     enableMapOverlay($(this).find('.map-overlay'));
   });
 
   // clever form auto-fill
-  $("a.autofill").click(function () {
-
-    var $form = $($(this).attr("href")), // works only if href represents #id of some form
-      fillTarget = $(this).attr("fill-target"),
-      fillData = $(this).attr("fill-data");
-
-    var $target = $form.find("textarea[name=" + fillTarget + "]"), // get target element of the form
-      data = $("#" + fillData).text();
-
-    $target.text(data);
-
-    setTimeout(function(){
-      $form.find("input:first").focus();
-    }, 1500);
+  $('.autofill').click(function () {
+    fillFormByLink($(this));
   });
 })(jQuery);
